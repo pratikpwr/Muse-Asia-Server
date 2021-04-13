6d7f0bfd95f1ccbe73a0dab6a18f406004e32d7f
pratikpwr <pratiksatishpawar@gmail.com>
2021-04-13 20:12:29 +0530

    add, get, edit, delete post

--- controllers/anime.js
+++ controllers/anime.js
@@ -1,21 +1,168 @@
+const { validationResult } = require("express-validator");
+
+const Anime = require('../models/anime');
 
 exports.getAllAnime= (req, res, next) =>{
 
-    res.status(200).json({message: "Success" , statusCode: 200});
+    Anime.find().then((anime)=> {
+
+        throwError({
+            condition: !anime,
+            errMsg: 'Anime not Found!',
+            statusCode: 404
+        });
+
+        res.status(200).json({
+            message: "Success! List of all Anime." ,
+            statusCode: 200,
+            anime: anime
+        });
+    }).catch((err) => {
+        catchError({next: next, error: err});
+    });
 };
 
 exports.addAnime = (req, res, next) => {
-    res.status(200).json({message: "Success" , statusCode: 200});
+    const errors = validationResult(req);
+    throwError({
+        condition: !errors.isEmpty,
+        errMsg: "Validation Failed, Enter valid data.",
+        statusCode: 422,
+    });
+
+    const title = req.body.title;
+    const japTitle = req.body.japTitle;
+    const description = req.body.description;
+    const imageUrl = req.body.imageUrl;
+    const rating = req.body.rating;
+    const playlistId = req.body.playlistId;
+    const japPlaylistId = req.body.japPlaylistId;
+
+    const anime = new Anime({
+        title: title,
+        japTitle: japTitle,
+        description: description,
+        imageUrl: imageUrl,
+        rating: rating,
+        playlistId: playlistId,
+        japPlaylistId: japPlaylistId
+    });
+
+    anime.save().then((anime) => {
+        res.status(201).json({
+            message: "Success! Anime added." ,
+            statusCode: 201,
+            anime: anime
+        });
+    }).catch((err) => {
+        catchError({next: next, error: err});
+    });
 }
 
 exports.getAnime = (req, res, next) => {
-    res.status(200).json({message: "Success" , statusCode: 200});
+
+    const animeId = req.params.animeId;
+
+    Anime.findById(animeId).then((anime)=> {
+
+        throwError({
+            condition: !anime,
+            errMsg: 'Anime not Found!',
+            statusCode: 404
+        })
+
+        res.status(200).json({
+            message: "Success! Anime found." ,
+            statusCode: 200,
+            anime: anime
+        });
+    }).catch((err) => {
+        catchError({next: next, error: err});
+    });
 }
 
 exports.editAnime = (req, res, next) => {
-    res.status(200).json({message: "Success" , statusCode: 200});
+    const errors = validationResult(req);
+    throwError({
+        condition: !errors.isEmpty,
+        errMsg: "Validation Failed, Enter valid data.",
+        statusCode: 422,
+    });
+
+    const animeId = req.params.animeId;
+    const title = req.body.title;
+    const japTitle = req.body.japTitle;
+    const description = req.body.description;
+    const imageUrl = req.body.imageUrl;
+    const rating = req.body.rating;
+    const playlistId = req.body.playlistId;
+    const japPlaylistId = req.body.japPlaylistId
+
+    Anime.findById(animeId).then((anime)=>{
+        throwError({
+            condition: !anime,
+            errMsg: 'Anime not Found!',
+            statusCode: 404
+        });
+
+        anime.title = title;
+        anime.japTitle = japTitle;
+        anime.description = description;
+        anime.imageUrl = imageUrl;
+        anime.rating = rating;
+        anime.playlistId = playlistId;
+        anime.japPlaylistId = japPlaylistId;
+
+        return anime.save();
+    }).then((updatedAnime)=> {
+
+        throwError({
+            condition: !updatedAnime,
+            errMsg: 'Anime not Found!',
+            statusCode: 404
+        });
+
+        res.status(200).json({
+            message: "Success! Anime Updated." ,
+            statusCode: 200,
+            anime: updatedAnime
+        });
+    }).catch((err) => {
+        catchError({next: next, error: err});
+    });
 }
 
 exports.deleteAnime = (req, res, next) => {
-    res.status(200).json({message: "Success" , statusCode: 200});
+    const animeId = req.params.animeId;
+
+    Anime.findByIdAndDelete(animeId).then((anime)=> {
+
+        throwError({
+            condition: !anime,
+            errMsg: 'Anime not Found!',
+            statusCode: 404
+        });
+
+        res.status(200).json({
+            message: "Success! Anime Deleted." ,
+            statusCode: 200,
+        });
+    }).catch((err) => {
+        catchError({next: next, error: err});
+    });
 }
+
+const throwError = ({ condition, errMsg, statusCode }) => {
+    if (condition) {
+      const error = new Error(errMsg);
+      error.statusCode = statusCode;
+      throw error;
+    }
+  };
+  
+  const catchError = ({ next, error }) => {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  };
--- routes/anime.js
+++ routes/anime.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { body } = require("express-validator");
+
 const animeController = require('../controllers/anime');
 
 const router = express.Router();
@@ -7,13 +9,37 @@ const router = express.Router();
 router.get('/', animeController.getAllAnime);
 
 // POST /anime/add-anime - add anime
-router.post('/add-anime', animeController.addAnime);
+router.post(
+    '/add-anime',
+    [
+        body("title").trim().isLength({ min: 1 }),
+        body("description").trim().isLength({ min: 1 }),
+        body("japTitle").trim().isLength({ min: 1 }),
+        body("imageUrl").trim().isLength({ min: 1 }),
+        body("rating").trim().isLength({ min: 1 }),
+        body("playlistId").trim().isLength({ min: 1 }),
+        body("japPlaylistId").trim().isLength({ min: 1 }),
+    ],
+   animeController.addAnime
+);
 
 // GET /anime/:animeId - get specific anime
 router.get('/:animeId', animeController.getAnime);
 
 // PUT /anime/:animeId - edit specific anime
-router.put('/:animeId', animeController.editAnime);
+router.put(
+    '/:animeId',
+    [
+        body("title").trim().isLength({ min: 1 }),
+        body("description").trim().isLength({ min: 1 }),
+        body("japTitle").trim().isLength({ min: 1 }),
+        body("imageUrl").trim().isLength({ min: 1 }),
+        body("rating").trim().isLength({ min: 1 }),
+        body("playlistId").trim().isLength({ min: 1 }),
+        body("japPlaylistId").trim().isLength({ min: 1 }),
+    ],
+    animeController.editAnime
+);
 
 // DELETE /anime/:animeId - delete specific anime
 router.delete('/:animeId', animeController.deleteAnime);
